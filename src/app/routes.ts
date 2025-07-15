ab1c36d15dc54678b02962ec79adca493ca60d6f
SerjRoman <sergeyroman26@gmail.com>
2025-07-15 13:24:25 +0100

    endpoint connected to server

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { UserRoutes } from '@modules/User';
 import { QuizRoutes } from '@modules/Quiz';
+import { LanguageRouter } from '@modules/Language';
 
 const AppRouter = Router();
 
 AppRouter.use('/users', UserRoutes);
 AppRouter.use('/quizzes', QuizRoutes);
+AppRouter.use('/languages', LanguageRouter);
 
 export { AppRouter };
